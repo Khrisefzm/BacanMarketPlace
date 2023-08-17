2163baf661c2e4045f3166ec6f4c9f4bbc703469
Franchesca Zegarra <129009072+Khrisefzm@users.noreply.github.com>
2023-08-17 03:17:57 +0000

    Rediseño de form, se añade apartado de categorias y subcategorias

--- src/front/js/pages/AddProductForm.jsx
+++ src/front/js/pages/AddProductForm.jsx
@@ -5,60 +5,89 @@ export const AddProductForm = () => {
         <div className="container">
             <h1>Añadir producto:</h1>
             <form>
-                <div className="mb-3">
-                    <label className="form-label">Nombre del producto:</label>
-                    <input type="text" maxLength="50" className="form-control" />
+                {/* primera Fila */}
+                <div className="row">
+                    <div className="col-lg-6 col-sm-12 mb-3">
+                        <label className="form-label">Nombre del producto:</label>
+                        <input type="text" maxLength="50" className="form-control" />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-12 mb-3">
+                        <label className="form-label">Estado:</label>
+                        <select className="form-select" aria-label="Default select example">
+                            <option defaultValue>Selecciona una opción</option>
+                            <option value="1">Usado</option>
+                            <option value="2">Nuevo</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-12 mb-3">
+                        <label className="form-label">Tipo de transacción:</label>
+                        <select className="form-select" aria-label="Default select example">
+                            <option defaultValue>Selecciona una opción</option>
+                            <option value="1">Venta</option>
+                            <option value="2">Intercambio</option>
+                            <option value="3">Ambos</option>
+                        </select>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label className="form-label">Description: </label>
-                    <textarea className="form-control" rows="3"></textarea>
+                {/* Segunda Fila */}
+                <div className="row mb-3">
+                    <div className="col-lg-3 col-md-3 col-sm-12">
+                        <label className="form-label">Categoría:</label>
+                        <select className="form-select" aria-label="Default select example">
+                            <option defaultValue>Selecciona una opción</option>
+                            <option value="1">Libros</option>
+                            <option value="2">Videojuegos</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-12">
+                        <label className="form-label">Sub categoría:</label>
+                        <select className="form-select" aria-label="Default select example">
+                            <option defaultValue>Selecciona una opción</option>
+                            <option value="1">Categoría 1</option>
+                            <option value="2">Categoría 2</option>
+                            <option value="2">Categoría 3</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <label className="form-label">Lugares de entrega:</label>
+                        <input type="text" maxLength="50" className="form-control" />
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label className="form-label">Estado:</label>
-                    <select className="form-select" aria-label="Default select example">
-                        <option defaultValue>Selecciona una opción</option>
-                        <option value="1">Usado</option>
-                        <option value="2">Nuevo</option>
-                    </select>
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Tipo de transacción:</label>
-                    <select className="form-select" aria-label="Default select example">
-                        <option defaultValue>Selecciona una opción</option>
-                        <option value="1">Venta</option>
-                        <option value="2">Intercambio</option>
-                        <option value="3">Ambos</option>
-                    </select>
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Precio:</label>
-                    <input type="number" className="form-control" />
+                {/* Tercera Fila */}
+                <div className="row mb-3">
+                    <div className="col">
+                        <label className="form-label">Precio:</label>
+                        <div className="input-group">
+                            <span className="input-group-text">$</span>
+                            <input type="text" className="form-control" />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <label className="form-label">Medios de pago:</label>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="checkbox" value="option1" defaultChecked />
+                            <label className="form-check-label">Tarjeta de crédito</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="checkbox" value="option2" />
+                            <label className="form-check-label" >Paypal</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="checkbox" value="option2" />
+                            <label className="form-check-label" >Efectivo</label>
+                        </div>
+                    </div>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Intercambio por:</label>
                     <div className="input-group mb-3">
-                        <input type="text" className="form-control"  />
+                        <input type="text" className="form-control" />
                         <button className="btn btn-outline-secondary" type="button" id="button-addon2">+</button>
                     </div>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="1" defaultChecked />
-                    <label className="form-check-label">
-                        Tarjetas de crédito
-                    </label>
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="2" />
-                    <label className="form-check-label">
-                        PayPal
-                    </label>
                 </div>
                 <div className="mb-3">
-                    <label className="form-label">Lugares de entrega:</label>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control"  />
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">+</button>
-                    </div>
+                    <label className="form-label">Description: </label>
+                    <textarea className="form-control" rows="3"></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary">Añadir producto</button>
             </form>
