8828d8914396bd4ac3304330486a64629a52ec6d
Franchesca Zegarra <129009072+Khrisefzm@users.noreply.github.com>
2023-08-19 01:26:10 +0000

    resolviendo conflictos

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -2,25 +2,21 @@ import React from "react";
 
 export const Login = () => {
     return (
-       <div className="FormLogin">
-        <form action="" className="form">
-            <h1 className="title">Login</h1>
-
-            <div className="inputContainer">
-                <input type="text" className="input" placeholder="a" />
-                <label htmlFor="" className="label">Email</label>
+        <div className="container-fluid">
+            <div className="col-lg-3 col-md-6 col-sm-12  m-auto border rounded mt-5 p-3">
+                <form action="" className="form">
+                    <h1 className="text-center">Iniciar Sesión</h1>
+                    <div className="mb-3">
+                        <label className="form-label">Nombre de usuario: </label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Contraseña:</label>
+                        <input type="password" className="form-control" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Registrarse</button>
+                </form>
             </div>
-
-            <div className="inputContainer">
-                <input type="text" className="input" placeholder="a" />
-                <label htmlFor="" className="label">Password</label>
-            </div>
-
-            <input type="submit" className="submitBtn" value={"Login"} />
-
-        </form>
-
-       </div>
-    
+        </div>
     )
 }
