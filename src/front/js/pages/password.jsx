835f51c19acc86bf09134e9aabb4b2432b497c63
Franchesca Zegarra <129009072+Khrisefzm@users.noreply.github.com>
2023-08-19 01:45:57 +0000

    crear campos con bootstrap

--- src/front/js/pages/password.jsx
+++ src/front/js/pages/password.jsx
@@ -2,25 +2,21 @@ import React from "react";
 
 export const Password = () => {
     return (
-       <div className="FormPasswordReset">
-        <form action="" className="form">
-            <h1 className="title">Login</h1>
-
-            <div className="inputContainer">
-                <input type="text" className="input" placeholder="a" />
-                <label htmlFor="" className="label">New Password</label>
-            </div>
-
-            <div className="inputContainer">
-                <input type="text" className="input" placeholder="a" />
-                <label htmlFor="" className="label">Confirm Password</label>
-            </div>
-
-            <input type="submit" className="submitBtn" value={"Reset Password"} />
-
-        </form>
-
-       </div>
-    
+        <div className="container-fluid">
+        <div className="col-lg-3 col-md-6 col-sm-12  m-auto border rounded mt-5 p-3">
+            <form action="" className="form">
+                <h1 className="text-center">Restaurar contraseña</h1>
+                <div className="mb-3">
+                    <label className="form-label">Nueva contraseña: </label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Confirma tu contraseña:</label>
+                    <input type="password" className="form-control" />
+                </div>
+                <button type="submit" className="btn btn-primary">Restaurar</button>
+            </form>
+        </div>
+    </div>
     )
 }
